5d9dac3f48026061d290143c5a1062c24ca440e0
Martin Kimani <martinikimi7@gmail.com>
2025-10-11 17:23:59 +0300

    CRUD

--- core/serializers.py
+++ core/serializers.py
@@ -2,27 +2,27 @@ from rest_framework import serializers
 from .models import Project, Trade, Issue, Comment, Attachment
 
 class ProjectSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = Project
         fields ='__all__'
         
 class TradeSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = Trade
         fields ='__all__'
         
 class IssueSerializer(serializers.ModelSerializer):
-    class meta:
-        model =
+    class Meta:
+        model =Issue
         fields ='__all__'
         
 class CommentSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = Comment
         fields = '__all_'
         
 class AttachmentSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = Attachment
         fields ='__all__'
         
--- core/views.py
+++ core/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Project,Trade,Issue,Comment, Attachment
+from .serializers import ProjectSerializer, TradeSerializer, IssueSerializer, CommentSerializer, AttachmentSerializer
 
-# Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    query_set = Project.object.all()
+    serializer_class = ProjectSerializer
+    
+class TradeViewSet(viewsets.ModelViewSet):
+    query_set = Trade.object.all()
+    serializer_class = TradeSerializer
+    
+class IssueViewSet(viewsets.ModelViewSet):
+    query_set = Issue.object.all()
+    serializer_class = IssueSerializer
+    
+class CommentViewSet(viewsets.ModelViewSet):
+    query_set = Comment.object.all()
+    Serializer_class = CommentSerializer
+    
+class AttachmentViewSet(viewsets.ModelViewSet):
+    query_set = Attachment.object.all()
+    Serializer_class = AttachmentSerializer
+    
+    
