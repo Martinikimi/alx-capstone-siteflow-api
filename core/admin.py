95d4d54ec521efb17665ba27d01b3c14819823b8
Martin Kimani <martinikimi7@gmail.com>
2025-10-11 16:36:09 +0300

    Admin registration

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import CustomUser, Project,Trade
+from .models import CustomUser, Project,Trade, Issue, Comment, Attachment
 
 admin.site.register(CustomUser)
 admin.site.register(Project)
 admin.site.register(Trade)
+admin.site.register(Issue)
+admin.site.register(Comment)
+admin.site.register(Attachment)
