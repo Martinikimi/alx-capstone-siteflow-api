70d18bfdf48d8b228c2da6376c7b18bdfc03b1e4
Martin Kimani <martinikimi7@gmail.com>
2025-10-11 15:47:34 +0300

    class Issue, Comments, Attachment migrations

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.conf import settings
 class CustomUser(AbstractUser):
     ROLE_CHOICES =[
         ('ADMIN', 'admin'),
@@ -29,10 +29,12 @@ class CustomUser(AbstractUser):
 
 
 TRADE_CHOICES =[
-        ('ARCHITECTURAL', 'architectural'),
+        ('GENERAL', 'general'),
+        ('ARCHITECTURAL','architectural'),
         ('STRUCTURAL', 'structural'),
         ('ELECTRICAL', 'electrical'),
         ('PLUMBING','plumbing'),
+        ('HVAC', 'hvac'),
     ]
 class Project(models.Model):
     project_name = models.CharField(max_length=200)
@@ -47,5 +49,34 @@ class Project(models.Model):
 class Trade(models.Model):
     name = models.CharField(max_length=100, choices= TRADE_CHOICES)
     
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
+
+class Issue(models.Model):
+    PRIORITY_STATUS =[
+        ('LOW','low'),
+        ('MEDIUM','medium'),
+        ('HIGH','high'),
+    ]
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    trade = models.CharField(max_length=200, choices= TRADE_CHOICES)
+    issue_title = models.CharField(max_length=100, blank=False, null=False)
+    detailed_description = models.TextField(blank=False, null =False)
+    priority = models.CharField(max_length=100, choices=PRIORITY_STATUS)
+    assigned_to = models.CharField(max_length=100,choices= TRADE_CHOICES)
+    due_date = models.DateField()
+    
+    def __str__(self):
+        return self.issue_title
+class Comment(models.Model):
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Attachment(models.Model):
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)  
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='attachments')
+    file = models.FileField(upload_to='attachments/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
