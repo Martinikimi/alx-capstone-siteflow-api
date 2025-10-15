37a1ffbe7a837dfb596c1187d6104e480765acb6
Martin Kimani <martinikimi7@gmail.com>
2025-10-15 17:24:54 +0300

    Add user registration endpoint with CustomUser model support

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import ProjectViewSet, TradeViewSet, IssueViewSet, CommentViewSet, AttachmentViewSet
+from .views import ProjectViewSet, TradeViewSet, IssueViewSet, CommentViewSet, AttachmentViewSet, register_user  
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -12,6 +12,7 @@ router.register(r'comments', CommentViewSet,)
 router.register(r'attachments', AttachmentViewSet,)
 
 urlpatterns = [
+    path('api/auth/register/', register_user, name='register'),
     path('api/auth/login/', TokenObtainPairView.as_view(), name='login'),
     path('api/', include(router.urls)),
 ]
--- core/views.py
+++ core/views.py
@@ -3,14 +3,42 @@ from rest_framework import viewsets
 from .models import Project,Trade,Issue,Comment, Attachment
 from .serializers import ProjectSerializer, TradeSerializer, IssueSerializer, CommentSerializer, AttachmentSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import get_user_model
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    # Get the data from the form
+    data = request.data
+    
+    # Create the user
+    user = User.objects.create_user(
+        username=data['username'],
+        email=data['email'],
+        password=data['password']
+    )
+    
+    # Add the role and specialty
+    user.role = data['role']
+    user.specialty = data.get('specialty', 'GENERAL')
+    user.save()
+    
+    return Response({
+        'message': 'User created!',
+        'user_id': user.id
+    })
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes =[IsAuthenticated]
-    
-    
-    
 class TradeViewSet(viewsets.ModelViewSet):
     queryset = Trade.objects.all()
     serializer_class = TradeSerializer
