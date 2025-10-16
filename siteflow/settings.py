adffdf44447e3a2da20e7aacc858af703101329d
Martin Kimani <martinikimi7@gmail.com>
2025-10-16 23:24:23 +0300

    Django_filters configuration

--- siteflow/settings.py
+++ siteflow/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'core',
     'corsheaders',
+    'django_filters',
 ]
 
 REST_FRAMEWORK ={
@@ -54,6 +55,12 @@ REST_FRAMEWORK ={
     'DEFAULT_RENDERER_CLASSES':
         ('rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',)
+        
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
 
 
